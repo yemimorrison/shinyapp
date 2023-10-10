76cad2b2af0393e0cf9aaf797c67bc637b9ba23d
Yemi Morrison <119580041+yemimorrison@users.noreply.github.com>
2023-10-10 19:31:06 +0200

    Update app.R

--- app.R
+++ app.R
@@ -6,7 +6,7 @@
 #' Find out more about building applications with Shiny here: http://shiny.rstudio.com/
 #'
 #'
-#' @param query_data_frame A dataframe containing the data with columns: Count, KPI, MunicipalityID, Municipality, Year, and UnemploymentRate.
+#' @param df A dataframe containing the data with columns: Count, KPI, MunicipalityID, Municipality, Year, and UnemploymentRate.
 #' @param filtered_df A dataframe created to plot the bar chart, and is filtered based on the year selected.
 #'
 #' @return a shiny web application plotting a basic bar chart of the unemployment rate in 6 municipalites. 
@@ -16,12 +16,12 @@
 #'
 #'
 
+install_github("yemimorrison/koladaAPI")
 library(shiny)
 library(dplyr)
 library(ggplot2)
 library(devtools)
 library(usethis)
-install_github("yemimorrison/koladaAPI")
 
 df <- getquerydata("http://api.kolada.se/v2/data/kpi/N03932", 
              entity = "municipality", 
@@ -30,43 +30,43 @@ df <- getquerydata("http://api.kolada.se/v2/data/kpi/N03932",
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Unemployment Rate in Sweden"),
-
-    # Sidebar with a drop-down input for Years
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("year", "Select Year:",
-                    choices = c("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"),
-                    selected = "2010")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("barplot")
-        )
+  
+  # Application title
+  titlePanel("Unemployment Rate in Sweden"),
+  
+  # Sidebar with a drop-down input for Years
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("year", "Select Year:",
+                  choices = c("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"),
+                  selected = "2010")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("barplot")
     )
+  )
 )
 
 # Define server logic required to draw a bar chart
 server <- function(input, output) {
-
+  
   output$barplot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-      selected_year <- as.numeric(input$year)
-      filtered_df <- query_data_frame %>% filter(Year == selected_year)
+    # generate bins based on input$bins from ui.R
+    selected_year <- as.numeric(input$year)
+    filtered_df <- df %>% filter(Year == selected_year)
     
-        # draw the barchart with the specified number of bins
-      bar_chart <-ggplot(filtered_df, aes(x = Municipality, y = UnemploymentRate))+
-        geom_bar(stat = "identity", fill = "orange") + 
-        labs(title = paste("Unemployment Rate in ", selected_year),
-             x = "Municipality",
-             y = "Unemployment Rate")+
-        theme_minimal()
-      
-      bar_chart
-    })
+    # draw the barchart with the specified number of bins
+    bar_chart <-ggplot(filtered_df, aes(x = Municipality, y = UnemploymentRate))+
+      geom_bar(stat = "identity", fill = "orange") + 
+      labs(title = paste("Unemployment Rate in ", selected_year),
+           x = "Municipality",
+           y = "Unemployment Rate")+
+      theme_minimal()
+    
+    bar_chart
+  })
 }
 
 # Run the application 
